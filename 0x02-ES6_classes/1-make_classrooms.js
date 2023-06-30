74f9f0bfe397e2d7a0673d5b33da15da8f5f3489
OduOduE <theoduemmanuel@gmail.com>
2023-06-30 22:46:19 +0000

    h

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const roomz = [19, 20, 34]
-  return roomz.map(size => new ClassRoom(size));
+  const roomz = [19, 20, 34];
+  return roomz.map((size) => new ClassRoom(size));
 }
