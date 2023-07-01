68de1ac76b4b0ac690d30d69003fc821bcefeee8
OduOduE <theoduemmanuel@gmail.com>
2023-07-01 13:44:34 +0000

    4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency'
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(naira) {
+    this._amount = naira;
+  }
+
+  get currency() {
+    return this._amount;
+  }
+
+  set currency(dollars) {
+    this._amount = dollars;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
