473275ffb4320dafc3e6139b932ee240d002ec3f
OduOduE <theoduemmanuel@gmail.com>
2023-07-04 21:26:24 +0000

    allDone

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,6 @@
 export default function hasValuesFromArray(set, array) {
   let verdict = false;
-  for (const x of array) {
-  // array.map((x) => {
+  array.map((x) => {
     if (set.has(x)) {
       verdict = true;
     }
