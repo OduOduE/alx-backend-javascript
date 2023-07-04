cc6044cb5babb6ab04fcc029dd8f1872b8c1c23c
OduOduE <theoduemmanuel@gmail.com>
2023-07-04 21:30:04 +0000

    allDone

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,10 @@
+/* eslint-disable array-callback-return */
 export default function hasValuesFromArray(set, array) {
   let verdict = false;
   array.map((x) => {
     if (set.has(x)) {
       verdict = true;
     }
-  }
+  });
   return verdict;
 }
