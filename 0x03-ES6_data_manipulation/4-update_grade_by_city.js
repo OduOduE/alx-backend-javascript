b4edd3c2dd884812b6d78b5e97eafaea83d44941
OduOduE <theoduemmanuel@gmail.com>
2023-07-04 18:16:04 +0000

    added4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,22 @@
-export default function updateStudentGradeByCity(list, city, newGrades) {
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-prototype-builtins */
+export default function updateStudentGradeByCity(list, city, newGrade) {
   return list
-    .filter((student) => student.location === city)
+    .filter((obj) => obj.location === city)
     .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.StudentId === student.id);
-      const grade = gradeObj ? gradeObj.grade : 'N/A';
-    return { ...student, grade };
+    //   console.log(newGrade);
+      newGrade.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          // eslint-disable-next-line no-param-reassign
+          student.grade = studentGrade.grade;
+        }
+
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
+        return student;
+      });
+
+      return student;
     });
 }
